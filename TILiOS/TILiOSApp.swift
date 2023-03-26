6850146be2d79aaba72c38ec868cf3ca3668b03b
Eduard <ptushko@gmail.com>
2023-03-26 12:49:32 +0300

    Initial Commit

---
+++ TILiOS/TILiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TILiOSApp.swift
+//  TILiOS
+//
+//  Created by Eduard on 26.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TILiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
